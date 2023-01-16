ad5a090890cd190517171adfed8e3851f8dff87a
Lorena <blorena1321@gmail.com>
2023-01-16 11:15:31 -0300

    js cartas

--- js/script.js
+++ js/script.js
@@ -15,20 +15,20 @@ function QuantidadeDeCartas(){
 const tabuleiro = document.querySelector('.tabuleiro'); // busca a div mae
 
 const passaros = [  // cada gif
-    'imagens/parrot1.gif',
-    'imagens/parrot1.gif',
-    'imagens/parrot2.gif',
-    'imagens/parrot2.gif',
-    'imagens/parrot3.gif',
-    'imagens/parrot3.gif',
-    'imagens/parrot4.gif',
-    'imagens/parrot4.gif',
-    'imagens/parrot5.gif',
-    'imagens/parrot5.gif',
-    'imagens/parrot6.gif',
-    'imagens/parrot6.gif',
-    'imagens/parrot7.gif',
-    'imagens/parrot7.gif',
+    './imagens/parrot1.gif',
+    './imagens/parrot1.gif',
+    './imagens/parrot2.gif',
+    './imagens/parrot2.gif',
+    './imagens/parrot3.gif',
+    './imagens/parrot3.gif',
+    './imagens/parrot4.gif',
+    './imagens/parrot4.gif',
+    './imagens/parrot5.gif',
+    './imagens/parrot5.gif',
+    './imagens/parrot6.gif',
+    './imagens/parrot6.gif',
+    './imagens/parrot7.gif',
+    './imagens/parrot7.gif',
 ];
 
 // let indice = numero;
@@ -41,13 +41,16 @@ function adicionarPassaros(){
 
     main.innerHTML = main.innerHTML + ` 
     <div data-test="card" class="cards"  onclick="clicarCards(this)">
+
     <div class="front-face face">
     <img data-test="face-up-image src= "${novoPassaro.value}"/>
     </div>
 
     <div class="back-face face" >
     <img data-test="face-down-image" src= "./imagens/back.png"/>
-    </div>`
+    </div>
+    </div>`;
+
 
 }
 
@@ -60,6 +63,7 @@ function adicionarPassarosNoDOM(passaro){
     for(let i = 0; i < pegarNCartas; i++){
         let template = ` 
         <div data-test="card" class="cards" onclick="clicarCards(this)" >
+
         <div class="front-face face" >
         <img data-test="face-up-image" src= "${passaros[i]}"/>
         </div>
@@ -70,37 +74,56 @@ function adicionarPassarosNoDOM(passaro){
         </div>
         </div>
         `;
+        
         main.innerHTML = main.innerHTML + template;
     }
-
-
-}
-
-function comparador() {
-    return Math.random() - 0.5;  //embaralhar
+    
 
 }
 
 adicionarPassarosNoDOM();
 //criar uma nova carta
 
-function MudarNomeCarta(){
-const carta = document.querySelector('.cards');
-carta.setAttribute('data-passaro', passaro);
+function comparador() {
+    return Math.random() - 0.5;  //embaralhar
+
 }
+
 
 let firstCard = '';
 let secondCard = '';
 
 function VerificarCartasIguais(){
+    const primeiraCarta = firstCard.getAttribute('img');
+    const segundaCarta = secondCard.getAttribute('img');
 
+    if (primeiraCarta === segundaCarta){
+        firstCard.classList.remove('front-face');
+        secondCard.classList.remove('front-face');
+
+    } else {
+
+        setTimeout(() => {
+            firstCard.classList.remove('reveal-card');
+            secondCard.classList.remove('reveal-card');
+
+            firstCard = '';
+            secondCard = '';
+   
+        }, 500);
+        
+        
+    }
 
 }
+
 
  
 function clicarCards(cartaClicada) {
     //revela carta clicada 
-    cartaClicada.className.includes('reveal-card');
+    if (cartaClicada.parentNode.className.includes('reveal-card')){
+        return;
+    }
 
     if(firstCard === ''){
         cartaClicada.classList.add('reveal-card');
@@ -108,12 +131,13 @@ function clicarCards(cartaClicada) {
     } else if (secondCard === '') {
         cartaClicada.classList.add('reveal-card');
         secondCard = cartaClicada;
-
-        VerificarCartasIguais();
-
+        
     }
+    VerificarCartasIguais();
 
 }
+
+
 
 // começo jogo (falta ainda)
 const game = () => { 
